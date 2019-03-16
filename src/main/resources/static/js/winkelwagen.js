0927b42a485476041d07be30a67a966cfff91725
Niekham <Niekham@users.noreply.github.com>
2019-03-16 16:53:52 +0100

    Winkelwagen html/css

--- src/main/resources/static/js/winkelwagen.js
+++ src/main/resources/static/js/winkelwagen.js
@@ -9,3 +9,23 @@ function addToCart(data) {
         sessionStorage.setItem("bestellingregel", JSON.stringify(jsonStorage));
     }
 }
+
+
+function showWinkelwagen(){
+    let modal = document.querySelector(".winkelwagen");
+    modal.style.display = "block";
+
+    document.querySelector(".close").addEventListener("click", function () {
+        document.querySelector(".winkelwagen").style.display = "none";
+    });
+    window.addEventListener("click", function () {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+}
+
+
+function winkelWagenContent(){
+    console.log(sessionStorage.getItem("product"))
+}
