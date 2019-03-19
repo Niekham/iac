bd88719e71b9387dfc3608a62781bea2e7c09558
Jonathan <Jonathan.deGier@student.hu.nl>
2019-03-19 19:28:14 +0100

    getting ready for pull

--- src/main/java/nl/hu/iac/webshop/config/SecurityConfiguration.java
+++ src/main/java/nl/hu/iac/webshop/config/SecurityConfiguration.java
@@ -28,6 +28,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         "/registreren**",
                         "/**",
                         "/products**",
+                        "/afrekenen**",
                         "/login**").permitAll()
                 .anyRequest().authenticated()
                 .and()
