1bced87fb72d88fd54c58a3965a96e2fe0d70f70
Jonathan <Jonathan.deGier@student.hu.nl>
2019-03-22 11:08:16 +0100

    /afrekenen werkt weer

--- src/main/java/nl/hu/iac/webshop/restControllers/AccountRestController.java
+++ src/main/java/nl/hu/iac/webshop/restControllers/AccountRestController.java
@@ -1,13 +1,12 @@
 package nl.hu.iac.webshop.restControllers;
-        import nl.hu.iac.webshop.domain.Account;
+import nl.hu.iac.webshop.domain.Account;
 import nl.hu.iac.webshop.domain.Adres;
 import nl.hu.iac.webshop.domain.Klant;
 import nl.hu.iac.webshop.services.AccountService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 
 @RestController
 @RequestMapping(AccountRestController.BASE_URL)
@@ -18,6 +17,13 @@ public class AccountRestController {
 
     public AccountRestController(AccountService accountService) {
         this.accountService = accountService;
+    }
+
+    @RequestMapping(value = "/details", method = RequestMethod.GET)
+    @ResponseBody
+    public Klant currentUserNameSimple(HttpServletRequest request) {
+        Principal principal = request.getUserPrincipal();
+        return accountService.getAccountByUsername(principal.getName()).getKlant();
     }
 
     @GetMapping("/{id}")
