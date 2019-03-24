bdf148ab0e5f747a8c019f5eccb5486d44b5d0c5
Niekham <Niekham@users.noreply.github.com>
2019-03-24 21:28:44 +0100

    add categorie to product

--- src/main/resources/static/js/categorie_toevoegen.js
+++ src/main/resources/static/js/categorie_toevoegen.js
@@ -34,50 +34,52 @@ function categorien(){
         })
         .then(function(myJson){
             for(const categorie of myJson){
-                var table = document.querySelector("table");
-                var row = table.insertRow(1);
-                var cell1 = row.insertCell(0);
-                var cell2 = row.insertCell(1);
-                var cell3 = row.insertCell(2);
-                cell1.innerHTML = categorie.id;
-                cell2.innerHTML = categorie.naam ;
-                cell3.innerHTML = categorie.omschrijving;
+                if (categorie.naam !== "Nieuw") {
+                    var table = document.querySelector("table");
+                    var row = table.insertRow(1);
+                    var cell1 = row.insertCell(0);
+                    var cell2 = row.insertCell(1);
+                    var cell3 = row.insertCell(2);
+                    cell1.innerHTML = categorie.id;
+                    cell2.innerHTML = categorie.naam;
+                    cell3.innerHTML = categorie.omschrijving;
 
-                var btn = document.createElement('input');
-                btn.type = "button";
-                btn.className = "btn";
-                btn.value="Toevoegen aan product";
-                row.appendChild(btn);
-                var button = document.querySelector(".btn")
-
-
-                    button.addEventListener("click", function(){
-                            let input1 = sessionStorage.getItem("productnaam");
-                            let input2 = sessionStorage.getItem("productprijs");
-                            let input3 = sessionStorage.getItem("productafbeelding");
-                            var text = '{"naam":"'+input1+'","prijs":"'+input2+'","afbeelding":"'+input3+'"}';
-                            let obj = JSON.parse(text);
-                            $.ajax({
-                                'url': 'http://localhost:8081/api/products/get_product',
-                                'data': JSON.stringify(obj),
-                                'type': 'POST',
-                                'contentType': 'application/json',
-                                'success': function (data) {
-                                    $.ajax({
-                                        'url': 'http://localhost:8081/api/products/add_categorie/'+data.id+'/'+categorie.id,
-                                        'data': JSON.stringify(obj),
-                                        'type': 'POST',
-                                        'contentType': 'application/json',
-                                        'success' : function(){
-                                                window.location.href = "http://localhost:8081/"
-                                        },
-
-                                    });
-                                }
-                            });
+                    var btn = document.createElement('input');
+                    btn.type = "button";
+                    btn.className = "btn";
+                    btn.value = "Toevoegen aan product";
+                    row.appendChild(btn);
+                    var button = document.querySelector(".btn");
+                }
 
 
+                    button.addEventListener("click", function () {
+                        let input1 = sessionStorage.getItem("productnaam");
+                        let input2 = sessionStorage.getItem("productprijs");
+                        let input3 = sessionStorage.getItem("productafbeelding");
+                        var text = '{"naam":"' + input1 + '","prijs":"' + input2 + '","afbeelding":"' + input3 + '"}';
+                        let obj = JSON.parse(text);
+                        $.ajax({
+                            'url': 'http://localhost:8081/api/products/get_product',
+                            'data': JSON.stringify(obj),
+                            'type': 'POST',
+                            'contentType': 'application/json',
+                            'success': function (data) {
+                                $.ajax({
+                                    'url': 'http://localhost:8081/api/products/add_categorie/' + data.id + '/' + categorie.id,
+                                    'data': JSON.stringify(obj),
+                                    'type': 'POST',
+                                    'contentType': 'application/json',
+                                    'success': function () {
+                                        window.location.href = "http://localhost:8081/"
+                                    },
+
+                                });
+                            }
                         });
+
+
+                    });
 
             }
         });
